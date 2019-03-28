f8d41fc81c6ddd8ddb90afafc172defc97aaf489
Scott Wolchok <swolchok@fb.com>
2019-03-28 13:30:32 -0700

    Update regex & scopeguard dependencies and run cargo update
    Summary: Bumping versions of dependencies. Notably, the regex crate has had several bugfixes and now includes SIMD by default.
    
    Reviewed By: adamjernst
    
    Differential Revision: D14670253
    
    fbshipit-source-id: 61b56af2fedff1105a963b3b31f02e92f8b3a8a3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-/**
- * Copyright 2018-present, Facebook, Inc.
+/*
+ * Copyright 2018-present Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #[cfg(test)]
 extern crate assert_cli;
 #[macro_use]
@@ -214,9 +213,9 @@ struct DisplayWarning<'a> {
 
 impl<'a> fmt::Display for DisplayWarning<'a> {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(fmt, "Warning: {}", self.inner.cause())?;
+        writeln!(fmt, "Warning: {}", self.inner.as_fail())?;
 
-        for e in self.inner.causes().skip(1) {
+        for e in self.inner.iter_chain().skip(1) {
             writeln!(fmt, "Caused by: {}", e)?;
         }
 
